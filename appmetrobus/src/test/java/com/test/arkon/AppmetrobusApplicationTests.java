b742a6608184458ae9e2bdf93d5af8e8f3aab124
Alekz <AlekzBellamy@users.noreply.github.com>
2022-04-20 23:54:12 -0500

    base spring boot api
    se inicia el proyecto de spring  para generar la api REST

---
+++ appmetrobus/src/test/java/com/test/arkon/AppmetrobusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.arkon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmetrobusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
